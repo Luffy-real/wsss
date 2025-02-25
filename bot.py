2ea41f535458acaa7083696a7f77a10d9e7e37d2
Luffy <awasthiaradhya308@gmail.com>
2025-02-25 20:52:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ from datetime import datetime
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 name ="""
- ＡＮＩＭＥ ＳＴＯＲＥ
 """
 
 class Bot(Client):
@@ -43,7 +42,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Store_Discussion for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -53,14 +52,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in Database, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Store_Discussion for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\nCreated by...\nhttps://t.me/Animes_Stores")
+        self.LOGGER(__name__).info(f"Bot Running..!\nCreated by...\n")
         self.LOGGER(__name__).info(f"""       
-
- ＡＮＩＭＥ ＳＴＯＲＥ
                                           """)
         self.username = usr_bot_me.username
         #web-response
