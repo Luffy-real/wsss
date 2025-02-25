8c9b65badb41e5abf0276ff0ee227664431ea1bc
Luffy <awasthiaradhya308@gmail.com>
2025-02-25 20:54:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "botofimmortal")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
